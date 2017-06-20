5550ef341ca50d66d21a70c563a8c09c0815f386
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-06-20 15:08:03 -0700

    triangle

---
+++ src/Shaders.metal
@@ -0,0 +1,20 @@
+//
+//  debug.metal
+//  vs-metal
+//
+//  Created by SATOSHI NAKAJIMA on 6/20/17.
+//  Copyright © 2017 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+vertex float4 basic_vertex(const device packed_float3* vertex_array [[ buffer(0) ]],
+                           unsigned int vid [[ vertex_id ]]) {
+  return float4(vertex_array[vid], 1.0);
+}
+
+fragment half4 basic_fragment() {
+  return half4(1.0);              
+}
+
--- src/VideoSessionController.swift
+++ src/VideoSessionController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 import AVFoundation
+import Metal
 
 class VideoSessionController: UIViewController {
   // Public properties
   var useFronCamera = false
   var fps:Int?
-
+ 
   // Calculated properties
   var cameraPosition:AVCaptureDevicePosition {
     return useFronCamera ? AVCaptureDevicePosition.front : AVCaptureDevicePosition.back
@@ -23,10 +24,80 @@ class VideoSessionController: UIViewController {
   private var session:AVCaptureSession?
   private var camera:AVCaptureDevice?
 
+  // Metal properties
+  let device = MTLCreateSystemDefaultDevice()!
+  let metalLayer:CAMetalLayer = {
+    let layer = CAMetalLayer()
+    layer.pixelFormat = .bgra8Unorm
+    layer.framebufferOnly = true
+    return layer
+  }()
+  var vertexBuffer:MTLBuffer?
+  
+  // Debug only for Metal
+  let vertexData:[Float] = [ 0.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0, -1.0, 0.0 ]
+  var pipelineState: MTLRenderPipelineState?
+  var commandQueue: MTLCommandQueue?
+  var timer: CADisplayLink?
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    metalLayer.device = self.device
+    view.layer.addSublayer(metalLayer)
+    
+    // Debug
+    let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+    vertexBuffer = device.makeBuffer(bytes:vertexData, length:dataSize, options:[])
+    // 1
+    let defaultLibrary = device.newDefaultLibrary()!
+    let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+    let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
+    // 2
+    let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+    pipelineStateDescriptor.vertexFunction = vertexProgram
+    pipelineStateDescriptor.fragmentFunction = fragmentProgram
+    pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+    
+    // 3
+    pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    commandQueue = device.makeCommandQueue()
+    timer = CADisplayLink(target: self, selector: #selector(VideoSessionController.gameloop))
+    timer?.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+    
     startVideoCaptureSession()
+  }
+  
+  func render() {
+    guard let drawable = metalLayer.nextDrawable(),
+          let pipelineState = self.pipelineState,
+          let commandBuffer = commandQueue?.makeCommandBuffer() else {
+      return
+    }
+    let renderPassDescriptor = MTLRenderPassDescriptor()
+    renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+    renderPassDescriptor.colorAttachments[0].loadAction = .clear
+    renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+    
+    let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+    renderEncoder.setRenderPipelineState(pipelineState)
+    renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
+    renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+    renderEncoder.endEncoding()
+    
+    commandBuffer.present(drawable)
+    commandBuffer.commit()
+  }
+  
+  func gameloop() {
+    autoreleasepool {
+      self.render()
+    }
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    metalLayer.frame = view.layer.frame
   }
 
   override func didReceiveMemoryWarning() {
