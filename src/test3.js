f7b09ac7b97d4318364492f87827628d72de8774
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-06-26 10:10:42 -0700

    hipass

--- src/test3.js
+++ src/test3.js
@@ -1,10 +1,29 @@
 {
     "pipeline":[{
-        "name":"sobel",
+        "name":"gaussianblur",
+        "attr":{
+            "sigma": [2.0]
+        }
+    },{
+        "name":"fork",
+    },{
+        "name":"gaussianblur",
+        "attr":{
+            "sigma": [4.0]
+        }
+    },{
+        "name":"invert",
+    },{
+        "name":"alpha",
+        "attr":{
+            "ratio": [0.5]
+        }
     },{
         "name":"boolean",
         "attr":{
-            "color2": [1.0, 1.0, 0.0, 1.0]
+            "range": [0.0, 0.5],
+            "color1": [1.0, 1.0, 1.0, 1.0],
+            "color2": [0.0, 0.0, 0.0, 1.0]
         }
     }]
 }
