b93506093de5953e91bfe0a9589aec559a43a21a
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-06-30 15:10:20 -0700

    cleanup: VSMPSFilter, unused code, private init

--- src/VSMPSFilter.swift
+++ src/VSMPSFilter.swift
@@ -24,13 +24,13 @@ struct VSMPSFilter: VSNode {
                 let kernel = MPSImageSobel(device: context.device, linearGrayColorTransform: weight)
                 return VSMPSFilter(kernel: kernel)
             }
-            /*
-             case "pyramid":
-             if let weight = params["weight"] as? [Float], weight.count == 3 {
-             let kernel = MPSImagePyramid(device: device)
-             return VSMPSFilter(kernel: kernel)
-             }
-             */
+        /*
+         case "pyramid":
+         if let weight = params["weight"] as? [Float], weight.count == 3 {
+         let kernel = MPSImagePyramid(device: device)
+         return VSMPSFilter(kernel: kernel)
+         }
+         */
         case "laplacian":
             let kernel = MPSImageLaplacian()
             return VSMPSFilter(kernel: kernel)
@@ -40,34 +40,10 @@ struct VSMPSFilter: VSNode {
         return nil
     }
     
-    init(kernel:MPSUnaryImageKernel) {
+    private init(kernel:MPSUnaryImageKernel) {
         self.kernel = kernel
     }
     
-    init(name:String, params paramsIn:[String:Any], context:VSContext) {
-        var params:[String:Any] = {
-            var params = [String:Any]()
-            if let info = VSScript.getNodeInfo(name: name),
-                let attrs = info["attr"] as? [[String:Any]] {
-                for attr in attrs {
-                    if let name=attr["name"] as? String,
-                       var defaults=attr["default"] as? [Float] {
-                        if let values = paramsIn[name] as? [Float], values.count <= defaults.count {
-                            print("overriding", name)
-                            for (index, value) in values.enumerated() {
-                                defaults[index] = value
-                            }
-                        }
-                        params[name] = defaults
-                    }
-                }
-            }
-            return params
-        }()
-        let sigma = params["sigma"] as! [Float]
-        kernel = MPSImageGaussianBlur(device: context.device, sigma: sigma[0])
-    }
-
     func encode(commandBuffer:MTLCommandBuffer, destination:VSTexture, context:VSContext) throws {
         let source = try context.pop()
         kernel.encode(commandBuffer: commandBuffer, sourceTexture: source.texture, destinationTexture: destination.texture)
