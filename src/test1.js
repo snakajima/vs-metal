f4e67932d1a1e3b0ad4343450cfb536171def378
Satoshi Nakajima <satoshi@uievolution.com>
2017-07-05 22:23:07 -0700

    hawaii, hipass

--- src/test1.js
+++ src/test1.js
@@ -1,11 +1,21 @@
 {
-    "title":"Emboss",
+    "title":"Gradient",
     "pipeline":[
-        { "name":"gaussian_blur", "attr":{"sigma":3.0} },
-        { "name":"sobel" },
-        { "name":"emboss", "attr":{"rotation":2.25}, "ui":{ "primary":["rotation"] } },
-        { "name":"color", "attr":{"color":[0.55, 0.55, 0.55, 1.0]} },
+        { "name":"gradient",
+            "attr":{"color1":[0.0, 0.0, 1.0, 1.0], "color2":[1.0, 0.0, 0.0, 1.0]} },
         { "name":"swap" },
-        { "name":"alpha" },
+        { "name":"gaussian_blur", "attr":{"sigma":2.0} },
+        { "name":"fork" },
+        { "name":"sobel" },
+        { "name":"canny_edge", "attr":{ "threshold":0.39, "thin":0.50 } },
+        { "name":"anti_alias" },
+        { "name":"swap" },
+        { "name":"toone", "ui":{ "hidden":["weight"] } },
+        { "name":"color" },
+        { "name":"alpha", "attr":{"ratio":0.5} },
+        { "name":"mono", "ui":{ "hidden":["weight"] } },
+        { "name":"swap" },
+        { "name":"alpha", "attr":{"ratio":0.10} },
+        { "name":"luminosity" }
     ]
 }
--- src/test2.js
+++ src/test2.js
@@ -1,103 +1,11 @@
 {
-  "title" : "Freeza",
-  "pipeline" : [
-    {
-      "name" : "gaussian_blur",
-      "attr" : {
-        "sigma" : 4
-      },
-      "ui" : {
-
-      }
-    },
-    {
-      "name" : "fork"
-    },
-    {
-      "name" : "gaussian_blur",
-      "attr" : {
-        "sigma" : 8
-      },
-      "ui" : {
-
-      }
-    },
-    {
-      "name" : "boolean",
-      "attr" : {
-        "range" : [
-          0.2944444,
-          0.6972222
-        ],
-        "color1" : [
-          0.95,
-          1,
-          1,
-          1
-        ],
-        "weight" : [
-          0.299,
-          0.587,
-          0.114
-        ],
-        "color2" : [
-          0,
-          0,
-          0,
-          1
-        ]
-      },
-      "ui" : {
-        "hidden" : [
-          "weight"
-        ]
-      }
-    },
-    {
-      "name" : "swap"
-    },
-    {
-      "name" : "sobel",
-      "attr" : {
-        "weight" : 2.088889
-      },
-      "ui" : {
-
-      }
-    },
-    {
-      "name" : "canny_edge",
-      "attr" : {
-        "color" : [
-          0.3333333,
-          0,
-          0.5861111,
-          1
-        ],
-        "thin" : 0,
-        "threshold" : 0.21
-      },
-      "ui" : {
-
-      }
-    },
-    {
-      "name" : "anti_alias",
-      "attr" : {
-
-      },
-      "ui" : {
-
-      }
-    },
-    {
-      "name" : "alpha",
-      "attr" : {
-        "ratio" : 1
-      },
-      "ui" : {
-
-      }
-    }
-  ]
+    "title":"Half Tone Gradient",
+    "pipeline":[
+        { "name":"halftone", "ui":{ "primary":["radius"] },
+            "attr":{ "color1":[0.5, 0.5, 0.5, 1.0], "radius":6.0 } },
+        { "name":"gradient",
+            "attr":{ "color1":[0.3, 0.3, 0.8, 1.0], "color2":[0.2, 0.8, 0.2, 1.00] } },
+        { "name":"swap" },
+        { "name":"luminosity" },
+    ]
 }
