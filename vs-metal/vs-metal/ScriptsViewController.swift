e3e01981c61b9e760b4456a4bb150120d1a9e3b3
Satoshi Nakajima <satoshi@uievolution.com>
2017-07-05 22:58:42 -0700

    smoothskin

--- vs-metal/vs-metal/ScriptsViewController.swift
+++ vs-metal/vs-metal/ScriptsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ScriptsViewController: UITableViewController {
     let filenames = ["test0", "test1", "test2", "test3", "test4", "test5", "test6",
-                     "hawaii", "hipass", "emboss", "freeza", "delicious", "sketch", "motion_detector", "mosaic", "motionblur", "blink", "red", "halftone", "gradientmap", "cartoon", "cartoon_hipass", "invalid"]
+                     "smoothskin", "hawaii", "hipass", "emboss", "freeza", "delicious", "sketch", "motion_detector", "mosaic", "motionblur", "blink", "red", "halftone", "gradientmap", "cartoon", "cartoon_hipass", "invalid"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
