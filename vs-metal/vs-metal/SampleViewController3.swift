18e9a7440e5360d1ab5e002a894eee31fb5feb33
Satoshi Nakajima <satoshi@uievolution.com>
2017-07-07 22:51:04 -0700

    importMovie

--- vs-metal/vs-metal/SampleViewController3.swift
+++ vs-metal/vs-metal/SampleViewController3.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 import MetalKit
+import MobileCoreServices
 
 class SampleViewController3: UIViewController {
     var context:VSContext = VSContext(device: MTLCreateSystemDefaultDevice()!)
     var runtime:VSRuntime?
-    lazy var session:VSCaptureSession = VSCaptureSession(context: self.context)
     lazy var renderer:VSRenderer = VSRenderer(context:self.context)
 
     override func viewDidLoad() {
@@ -22,13 +22,27 @@ class SampleViewController3: UIViewController {
             mtkView.device = context.device
             mtkView.delegate = self
             context.pixelFormat = mtkView.colorPixelFormat
-            
-            // This is an alternative way to create a script object (Beta)
-            let script = VSScript()
-                .mono()
-            runtime = script.compile(context: context)
+        }
+        
+        // https://stackoverflow.com/questions/12500408/can-i-use-avfoundation-to-stream-downloaded-video-frames-into-an-opengl-es-textu/12500409#12500409
+    }
+    
+    @IBAction func importMovie(_ sender:UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = [kUTTypeMovie as String]
+        picker.videoQuality = .typeHigh
+        self.present(picker, animated: true, completion: nil)
+    }
+}
 
-            session.start()
+extension SampleViewController3 : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("didFinish")
+        self.dismiss(animated: true, completion: nil)
+        if let url = info[UIImagePickerControllerMediaURL] {
+            print("didFinish", url)
         }
     }
 }
