5f8415a11102aaff4e9e6ec13e258e8a4d178e8d
Satoshi Nakajima <satoshi@uievolution.com>
2017-07-14 04:37:36 -0700

    mtkView outlet for sample1

--- vs-metal/vs-metal/SampleViewController1.swift
+++ vs-metal/vs-metal/SampleViewController1.swift
@@ -10,6 +10,7 @@ import UIKit
 import MetalKit
 
 class SampleViewController1: UIViewController {
+    @IBOutlet var mtkView:MTKView!
     var context:VSContext = VSContext(device: MTLCreateSystemDefaultDevice()!)
     lazy var session:VSCaptureSession = VSCaptureSession(device: self.context.device, pixelFormat: self.context.pixelFormat, delegate: self.context)
     var runtime:VSRuntime?
@@ -18,30 +19,28 @@ class SampleViewController1: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let mtkView = self.view as? MTKView {
-            mtkView.device = context.device
-            mtkView.delegate = self
-            context.pixelFormat = mtkView.colorPixelFormat
-            
-            // This is a VideoShader script, which represents a cartoon filter
-            let json = [
-                "pipeline":[
-                    [ "name":"gaussian_blur", "attr":["sigma": 2.0] ],
-                    [ "name":"fork" ],
-                    [ "name":"gaussian_blur", "attr":["sigma": 2.0] ],
-                    [ "name":"toone" ],
-                    [ "name":"swap" ],
-                    [ "name":"sobel"],
-                    [ "name":"canny_edge", "attr":["threshold": 0.19, "thin": 0.50] ],
-                    [ "name":"anti_alias" ],
-                    [ "name":"alpha" ],
-                ]
+        mtkView.device = context.device
+        mtkView.delegate = self
+        context.pixelFormat = mtkView.colorPixelFormat
+        
+        // This is a VideoShader script, which represents a cartoon filter
+        let json = [
+            "pipeline":[
+                [ "name":"gaussian_blur", "attr":["sigma": 2.0] ],
+                [ "name":"fork" ],
+                [ "name":"gaussian_blur", "attr":["sigma": 2.0] ],
+                [ "name":"toone" ],
+                [ "name":"swap" ],
+                [ "name":"sobel"],
+                [ "name":"canny_edge", "attr":["threshold": 0.19, "thin": 0.50] ],
+                [ "name":"anti_alias" ],
+                [ "name":"alpha" ],
             ]
-            let script = VSScript(json: json)
-            runtime = script.compile(context: context)
-            
-            session.start()
-        }
+        ]
+        let script = VSScript(json: json)
+        runtime = script.compile(context: context)
+        
+        session.start()
     }
 }
 
