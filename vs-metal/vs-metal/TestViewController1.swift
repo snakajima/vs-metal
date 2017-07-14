f11db7643eed8dec1ba70d8060314cafeeec4890
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-07-14 11:45:33 -0700

    test1

--- vs-metal/vs-metal/TestViewController1.swift
+++ vs-metal/vs-metal/TestViewController1.swift
@@ -22,28 +22,23 @@ class TestViewController1: UIViewController {
     }
     var startTime:CMTime?
     
-    var context:VSContext = VSContext(device: MTLCreateSystemDefaultDevice()!)
-    var runtime:VSRuntime?
+    var device = MTLCreateSystemDefaultDevice()!
+    var texture:MTLTexture?
     var writer:VSVideoWriter?
-    lazy var session:VSCaptureSession = VSCaptureSession(device: self.context.device, pixelFormat: self.context.pixelFormat, delegate: self)
+    lazy var session:VSCaptureSession = VSCaptureSession(device: self.device, pixelFormat: self.mtkView.colorPixelFormat, delegate: self)
 
     // For rendering
-    lazy var commandQueue:MTLCommandQueue = self.context.device.makeCommandQueue()
-    lazy var renderer:VSRenderer = VSRenderer(device:self.context.device, pixelFormat:self.context.pixelFormat)
+    lazy var commandQueue:MTLCommandQueue = self.device.makeCommandQueue()
+    lazy var renderer:VSRenderer = VSRenderer(device:self.device, pixelFormat:self.mtkView.colorPixelFormat)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         recording = false // force the UI update
 
-        mtkView.device = context.device
+        mtkView.device = device
         mtkView.delegate = self
         mtkView.transform = (session.cameraPosition == .front) ? CGAffineTransform(scaleX: -1.0, y: 1.0) : CGAffineTransform.identity
-        context.pixelFormat = mtkView.colorPixelFormat
         
-        let script = VSScript()
-            .gaussian_blur(sigma: 5.0)
-        runtime = script.compile(context: context)
-
         session.start()
     }
     
@@ -74,44 +69,35 @@ extension TestViewController1 : VSVideoWriterDelegate {
 
 extension TestViewController1 : VSCaptureSessionDelegate {
     func didCaptureOutput(session:VSCaptureSession, texture textureIn:MTLTexture, sampleBuffer:CMSampleBuffer, presentationTime:CMTime) {
-        self.context.set(texture: textureIn, sampleBuffer: sampleBuffer)
-        if let commandBuffer = self.runtime?.encode(commandBuffer:self.context.makeCommandBuffer(), context:self.context) {
-            commandBuffer.addCompletedHandler { (_) in
-                DispatchQueue.main.async {
-                    self.context.textureOut  = self.context.pop() // store it for renderer
-                    self.context.flush()
-                    guard let textureOut = self.context.textureOut?.texture else {
-                        return
-                    }
-                    if self.writer == nil {
-                        print("Sample3: creating a new writer")
-                        self.writer = VSVideoWriter(delegate: self)
-                        let size = CGSize(width: textureOut.width, height: textureOut.height)
-                        let _ = self.writer?.prepare(size: size)
-                    }
-                    if self.recording {
-                        if self.startTime == nil {
-                            print("Sample3: start the recording session")
-                            self.startTime = presentationTime
-                            var transform = CGAffineTransform.identity
-                            switch(UIDevice.current.orientation) {
-                            case .portrait:
-                                transform = transform.rotated(by: .pi/2.0)
-                            case .landscapeLeft:
-                                transform = transform.rotated(by: .pi)
-                            case .portraitUpsideDown:
-                                transform = transform.rotated(by: -.pi/2.0)
-                            default:
-                                break
-                            }
-                            self.writer?.set(transform: transform)
-                            self.writer?.startSession(atSourceTime: presentationTime)
-                        }
-                        self.writer?.append(texture: textureOut, presentationTime: presentationTime)
-                    }
-                }
+        // Intentionally adding async
+        DispatchQueue.main.async {
+            self.texture = textureIn
+            if self.writer == nil {
+                print("Sample3: creating a new writer")
+                self.writer = VSVideoWriter(delegate: self)
+                let size = CGSize(width: textureIn.width, height: textureIn.height)
+                let _ = self.writer?.prepare(size: size)
             }
-            commandBuffer.commit()
+            if self.recording {
+                if self.startTime == nil {
+                    print("Sample3: start the recording session")
+                    self.startTime = presentationTime
+                    var transform = CGAffineTransform.identity
+                    switch(UIDevice.current.orientation) {
+                    case .portrait:
+                        transform = transform.rotated(by: .pi/2.0)
+                    case .landscapeLeft:
+                        transform = transform.rotated(by: .pi)
+                    case .portraitUpsideDown:
+                        transform = transform.rotated(by: -.pi/2.0)
+                    default:
+                        break
+                    }
+                    self.writer?.set(transform: transform)
+                    self.writer?.startSession(atSourceTime: presentationTime)
+                }
+                self.writer?.append(texture: textureIn, presentationTime: presentationTime)
+            }
         }
     }
 }
@@ -120,7 +106,7 @@ extension TestViewController1 : MTKViewDelegate {
     public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
     
     public func draw(in view: MTKView) {
-        if let texture = self.context.textureOut?.texture {
+        if let texture = self.texture {
             renderer.encode(commandBuffer:commandQueue.makeCommandBuffer(), view:view, texture: texture)?.commit()
         }
     }
