24a3bf0e7fac61de7a54191a74d7a3015ba16918
Satoshi Nakajima <satoshi@uievolution.com>
2017-08-02 22:07:41 -0700

    sensitibity

--- src/test6.js
+++ src/test6.js
@@ -1,17 +1,18 @@
 {
     "pipeline":[
                 { "name":"fork" },
+                { "name":"mono" },
                 { "name":"derivative" },
                 { "name":"gaussian_blur",
                 "attr":{
-                "sigma": [2.0],
+                "sigma": 2.0,
                 }
                 },
-                { "name":"harris_detector" },
-                { "name":"local_non_max_suppression" },
+                { "name":"harris_detector", "attr":{"sensitivity":10.0 } },
+                { "name":"local_non_max_suppression", "attr":{"threshold":0.1} },
                 { "name":"gaussian_blur",
                 "attr":{
-                "sigma": [4.0],
+                "sigma": 4.0,
                 }
                 },
                 { "name":"step" },
